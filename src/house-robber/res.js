904afe03ca163d856135288425466277e1978750
renyou.hdd <renyou.hdd@alibaba-inc.com>
2020-06-09 17:13:52 +0800

    update: 198 自底向上

--- src/house-robber/res.js
+++ src/house-robber/res.js
@@ -1,7 +1,9 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * 自顶向下
  */
+
 var rob = function (nums) {
   if (!nums || !nums.length) return 0;
   const dp = new Array(nums.length + 2).fill(0);
@@ -12,5 +14,17 @@ var rob = function (nums) {
   return dp[0];
 };
 
+
+// 自底向上
+var rob2 = function (nums) {
+  if (!nums || !nums.length) return 0;
+  const dp = new Array(nums.length + 2).fill(0);
+  const n = nums.length;
+  for (let i = 2; i < n + 2; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 2]);
+  }
+  return dp[n + 1];
+};
+
 const nums = [2, 7, 9, 3, 1];
-console.log('nums', rob(nums));
+console.log('nums', rob2(nums));
