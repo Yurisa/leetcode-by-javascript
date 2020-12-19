e2b6c3a6d5299aa9c674ac65619b63666d38f906
Yurisa <779509411@qq.com>
2020-12-19 15:19:23 +0800

    update: 76

--- src/minimum-window-substring/res.js
+++ src/minimum-window-substring/res.js
@@ -5,7 +5,7 @@
  */
 // 自己的方法 超过limit
 var minWindow = function (s, t) {
-  let window = '';
+  let window = "";
   let ret = s;
 
   if (!isContain(ret, t)) {
@@ -14,7 +14,7 @@ var minWindow = function (s, t) {
 
   for (let i = 0; i < s.length; i++) {
     const char = s[i];
-    window = window + char
+    window = window + char;
 
     while (isContain(window, t)) {
       ret = window.length <= ret.length ? window : ret;
@@ -27,17 +27,15 @@ var minWindow = function (s, t) {
       return false;
     }
 
-    let ret = true;
     for (let i = 0; i < t.length; i++) {
       const index = window.indexOf(t[i]);
       if (index === -1) {
-        ret = false;
-        break;
+        return false;
       } else {
-       window =  window.substring(0, index) + window.substring(index + 1);
+        window = window.substring(0, index) + window.substring(index + 1);
       }
     }
-    return ret;
+    return true;
   }
   return ret;
 };
@@ -47,6 +45,68 @@ const s =
 const t =
   "cjgamyzjwxrgwedhsexosmswogckohesskteksqgrjonnrwhywxqkqmywqjlxnfrayykqotkzhxmbwvzstrcjfchvluvbaobymlrcgbbqaprwlsqglsrqvynitklvzmvlamqipryqjpmwhdcsxtkutyfoiqljfhxftnnjgmbpdplnuphuksoestuckgopnlwiyltezuwdmhsgzzajtrpnkkswsglhrjprxlvwftbtdtacvclotdcepuahcootzfkwqhtydwrgqrilwvbpadvpzwybmowluikmsfkvbebrxletigjjlealczoqnnejvowptikumnokysfjyoskvsxztnqhcwsamopfzablnrxokdxktrwqjvqfjimneenqvdxufahsshiemfofwlyiionrybfchuucxtyctixlpfrbngiltgtbwivujcyrwutwnuajcxwtfowuuefpnzqljnitpgkobfkqzkzdkwwpksjgzqvoplbzzjuqqgetlojnblslhpatjlzkbuathcuilqzdwfyhwkwxvpicgkxrxweaqevziriwhjzdqanmkljfatjifgaccefukavvsfrbqshhswtchfjkausgaukeapanswimbznstubmswqadckewemzbwdbogogcysfxhzreafwxxwczigwpuvqtathgkpkijqiqrzwugtr";
 
-const s2 = 'acbbaca'
-const t2 = 'baca'
 console.log(minWindow(s, t));
+const s2 = "ADOBECODEBANC";
+const t2 = "ABC";
+
+var minWindow2 = function (s, t) {
+  let left = 0;
+  let right = 0;
+  let valid = 0; // 表示涵盖t的字符数
+  let start = 0;
+  let len = Number.MAX_VALUE;
+  const need = toMap(t);
+  const needLength = Object.keys(need).length;
+  const window = {};
+
+  while (right < s.length) {
+    const c = s[right];
+
+    if (need[c]) {
+      if (!window[c]) {
+        window[c] = 0;
+      }
+      window[c]++;
+      if (window[c] === need[c]) {
+        valid++;
+      }
+    }
+
+    while (valid === needLength) {
+      if (right - left + 1 < len) {
+        start = left;
+        len = right - left + 1;
+      }
+
+      let d = s[left];
+      left++;
+
+
+      if (need[d]) {
+        if (window[d] === need[d]) {
+          valid--;
+        }
+        window[d]--;
+      }
+    }
+
+    right++;
+
+  }
+
+  function toMap(t) {
+    const map = {};
+    for (let i = 0; i < t.length; i++) {
+      const char = t[i];
+      if (!map[char]) {
+        map[char] = 0;
+      }
+      map[char]++;
+    }
+    return map;
+  }
+
+  return len === Number.MAX_VALUE ? '' : s.substr(start, len);
+};
+
+console.log(minWindow2(s2, t2));
