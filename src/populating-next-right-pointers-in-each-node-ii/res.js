bc4a57234ac6c6f52fff4eae59c821a399039511
renyou.hdd <renyou.hdd@alibaba-inc.com>
2022-04-26 22:46:04 +0800

    update: 117

---
+++ src/populating-next-right-pointers-in-each-node-ii/res.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=117 lang=javascript
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function (root) {
+  if (root === null) return root;
+  const queue = [];
+  queue.push(root);
+
+  while (queue.length) {
+    let levelSize = queue.length;
+    let next = null;
+
+    while (levelSize--) {
+      const node = queue.pop();
+      node.next = next;
+      next = node;
+
+      node.right && queue.unshift(node.right);
+      node.left && queue.unshift(node.left);
+    }
+  }
+  return root;
+};
+// @lc code=end
