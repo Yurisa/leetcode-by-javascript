1808fa1eeed9419840818370148292e77e68aff1
Yurisa <779509411@qq.com>
2022-08-28 18:17:53 +0800

    516

---
+++ src/516.longest-palindromic-subsequence.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=516 lang=javascript
+ *
+ * [516] Longest Palindromic Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function (s) {
+  const n = s.length;
+  const dp = new Array(n);
+  for (let i = 0; i < n; i++) {
+    dp[i] = new Array(n).fill(0);
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2;
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+};
+// @lc code=end
