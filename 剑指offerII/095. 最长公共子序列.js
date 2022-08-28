4abb4d4d6ab6ee47286ea47c77f4421779030b8e
Yurisa <779509411@qq.com>
2022-08-28 21:06:46 +0800

    最小路径和

---
+++ 剑指offerII/095. 最长公共子序列.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length;
+  const n = text2.length;
+  const dp = new Array(m + 1);
+  for (let i = 0; i < m + 1; i++) {
+    dp[i] = new Array(n + 1).fill(0);
+  }
+  for (let i = 1; i < m + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[m][n];
+};
