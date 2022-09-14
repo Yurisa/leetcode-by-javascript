8537823ec1b71ed9c1b26c372b4b04ba87d9c276
Yurisa <779509411@qq.com>
2022-09-14 20:15:39 +0800

    chore: update

--- src/92.reverse-linked-list-ii.js
+++ src/92.reverse-linked-list-ii.js
@@ -18,8 +18,23 @@
  * @param {number} right
  * @return {ListNode}
  */
-var reverseBetween = function(head, left, right) {
-    
+var reverseBetween = function (head, left, right) {
+  if (left === 1) {
+    return reverseN(head, right);
+  }
+  head.next = reverseBetween(head.next, left - 1);
+  return head;
 };
-// @lc code=end
 
+let s = null;
+function reverseN(head, N) {
+  if (N === 1) {
+    return head;
+  }
+  let last = reverseN(head.next, N - 1);
+  head.next.next = head;
+  head.next = s;
+
+  return last;
+}
+// @lc code=end
--- src/reverse-linked-list-ii/res.js
+++ src/reverse-linked-list-ii/res.js
@@ -1,3 +1,10 @@
+/*
+ * @lc app=leetcode id=92 lang=javascript
+ *
+ * [92] Reverse Linked List II
+ */
+
+// @lc code=start
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -7,27 +14,28 @@
  */
 /**
  * @param {ListNode} head
- * @param {number} m
- * @param {number} n
+ * @param {number} left
+ * @param {number} right
  * @return {ListNode}
  */
-var reverseBetween = function (head, m, n) {
-  if (m === 1) {
-    return reverseN(head, n);
+var reverseBetween = function (head, left, right) {
+  if (left === 1) {
+    return reverseN(head, right);
   }
-  head.next = reverseBetween(head.next, m - 1, n - 1);
+  head.next = reverseBetween(head.next, left - 1, right - 1);
   return head;
 };
 
-let successor = null;
-function reverseN(head, n) {
-  if (n === 1) {
-    successor = head.next;
+let s = null;
+function reverseN(head, N) {
+  if (N === 1) {
+    s = head.next;
     return head;
   }
-
-  const last = reverseN(head.next, n - 1);
+  let last = reverseN(head.next, N - 1);
   head.next.next = head;
-  head.next = successor;
+  head.next = s;
+
   return last;
 }
+// @lc code=end
